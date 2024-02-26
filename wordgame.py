40f563f2c5cd8e05bdb0e3071b316da561498ac0
JustineXp <justineislearning@gmail.com>
2024-02-26 15:57:45 +0300

    User input logic with Regex

--- wordgame.py
+++ wordgame.py
@@ -1,4 +1,7 @@
+import re
 from wordclass import Word
+
+pattern = re.compile("[a-zA-Z]{5}")
 
 
 word = Word()
@@ -7,11 +10,23 @@ random_word = word.random_word
 first_letter = word.first_letter
 
 print(random_word)
-print(first_letter)
-print(all_letter_words)
 
 while True:
-    user_word = str(input('Guess a 5 letter word : '))
-    if len(user_word) == 5 and user_word == random_word:
-        print(user_word)
-        break
+    user_word = str(input('Guess a 5 letter word : ').strip())
+    if bool(re.search("[^a-zA-Z]", user_word)):
+        print('The word should have only letters, try again . . .')
+    elif bool(pattern.search(user_word)):
+        if user_word == random_word:
+            print(f'Congratulations, {user_word} is the correct word.')
+            confirmation = input('Could you wish to continue ? (yes/no) ')
+            if confirmation == 'yes':
+                word = Word()
+                user_word = str(input('Guess a 5 letter word : ').strip())
+            elif confirmation == 'no':
+                break
+            else:
+                print('Use Yes or No only.')
+        else:
+            print('Sorry that is not correct. Try again. . .')
+    else:
+        print('The word Entered is not 5 Letters Long.')
